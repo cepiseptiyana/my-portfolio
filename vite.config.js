f35b6815f209f24ef62eb5852c922b529ce1a6ea
Cepi Septiyana <cepifams3@gmail.com>
2026-09-22 20:45:52 +0700

    update portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio-testing/",
+  base: "/my-portfolio/",
   css: {
     preprocessorOptions: {
       sass: {
